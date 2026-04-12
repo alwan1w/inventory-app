746965b6fbd5502511fede948719fb1e8c830ac3
alwan1w <alwanfa49@gmail.com>
2026-04-12 20:18:18 +0700

    feat: setup models and relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+// Import relasi HasMany
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    //
+    // Mengizinkan kolom 'name' untuk diisi
+    protected $fillable = ['name'];
+
+    // Relasi: 1 Kategori punya BANYAK Barang
+    public function items(): HasMany
+    {
+        return $this->hasMany(Item::class);
+    }
 }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+// Import relasi BelongsTo
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Item extends Model
 {
-    //
+    // Mendaftarkan kolom yang boleh diisi ke database
+    protected $fillable = [
+        'category_id',
+        'sku',
+        'name',
+        'price',
+        'stock'
+    ];
+
+    // Relasi: 1 Barang MILIK 1 Kategori
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
